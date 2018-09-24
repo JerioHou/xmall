3998b225c0691559c00a3914ec558fd8417db5df
jerio_hou <houjie20@gmail.com>
2018-09-24 17:00:38 +0800

    商城前端

---
+++ xmall-portal-web/src/main/java/cn/jerio/portal/controller/ContentController.java
@@ -0,0 +1,30 @@
+package cn.jerio.portal.controller;
+
+import cn.jerio.content.service.ContentService;
+import cn.jerio.pojo.TbContent;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by Jerio on 2018/9/24
+ */
+@RestController
+@RequestMapping("/content")
+public class ContentController {
+
+    @Reference
+    private ContentService contentService;
+
+    /**
+     * 根据广告分类ID查询广告列表
+     * @param categoryId
+     * @return
+     */
+    @RequestMapping("/findByCategoryId")
+    public List<TbContent> findByCategoryId(Long categoryId) {
+        return contentService.findByCategoryId(categoryId);
+    }
+}
